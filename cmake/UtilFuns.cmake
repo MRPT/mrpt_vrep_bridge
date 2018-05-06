759ca02ba40ca7dea069fbdcbebc8876a9a357f5
Nikos Koukis <nickkouk@gmail.com>
2018-05-06 10:51:35 +0100

    CMake-configure doxygen documentation generation

---
+++ cmake/UtilFuns.cmake
@@ -0,0 +1,12 @@
+# Get a list of all the subdirectories of a given directory
+# See also: https://stackoverflow.com/a/7788165/2843583
+MACRO(SUBDIRLIST result curdir)
+  FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
+  SET(dirlist "")
+  FOREACH(child ${children})
+    IF(IS_DIRECTORY ${curdir}/${child})
+      LIST(APPEND dirlist ${child})
+    ENDIF()
+  ENDFOREACH()
+  SET(${result} ${dirlist})
+ENDMACRO()
